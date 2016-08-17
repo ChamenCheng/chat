eaa2c17ffe3c12bb96a221f4ee6ba22c50607481
ex_cenzw <zhuowen.cen@partner.midea.com.cn>
2016-08-17 16:16:19 +0800

    all

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var express = require('express'),
 
 app.set('port', 3000); // 设置端口号
 app.set('view engine', 'jade'); // 设置模板引擎
-app.use(bodyParser());
+app.use(bodyParser()); // 资源解析器
 app.use(methodOverride());
+app.use(express.static('public')); // 设置全局资源路径
 
 // 设置路由
 app.get('/', function(req, res) {
