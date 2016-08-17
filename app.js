d3d11646647f4da543b2de4ee779e7e8c18a1330
ex_cenzw <zhuowen.cen@partner.midea.com.cn>
2016-08-17 15:27:46 +0800

    添加文件

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override');
+
 
 app.set('port', 3000); // 设置端口号
 app.set('view engine', 'jade'); // 设置模板引擎
+app.use(bodyParser());
+app.use(methodOverride());
 
 // 设置路由
 app.get('/', function(req, res) {
-	res.send('hello');
+	res.render('index', {
+		title: 'Home',
+		message: 'test'
+	});
 });
 
 app.listen(app.get('port'));
