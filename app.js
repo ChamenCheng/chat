09e115f3470677c1f03fca13e1dbacae03dba9ae
浮尘 <757213882@qq.com>
2016-08-18 23:14:54 +0800

    修改设置引擎为ejs

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var express = require('express'),
 
 
 app.set('port', 3000); // 设置端口号
-app.set('view engine', 'jade'); // 设置模板引擎
+app.set('view engine', 'ejs'); // 设置模板引擎
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser()); // 资源解析器
 app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'public'))); // 设置全局资源路径
+app.use(express.static(__dirname + '/public')); // 设置全局资源路径
 
 app.use('/', routes); // 默认路由
 app.use('/login', login); // 登录路由
